4dde6796c571a3b50b1e69fe2d1f0278c1a0a912
Luis Corrales <Luiscorrales2508@gmail.com>
2022-10-15 01:05:37 -0300

    intento 3001

--- index.ts
+++ index.ts
@@ -4,23 +4,8 @@ import Server from "./classes/server";
 import defaultRoutes from "./routes/default.routes";
 import gameRoutes from "./routes/videojuegos.routes";
 import cors from "cors";
-import express from 'express'
 
-const app = express();
 const server = new Server();
-
-app.use(express.json());
-
-
-server.app.use(bodyParser.json());
-server.app.use(bodyParser.urlencoded({extended:true}))
-
-server.app.use('/', defaultRoutes);
-server.app.use('/videojuegos', gameRoutes);
-
-
-
-
 
 
 server.app.use(function (req, res, next) {
@@ -37,6 +22,19 @@ server.app.use(function (req, res, next) {
 
     next();
 });
+
+server.app.use(bodyParser.json());
+server.app.use(bodyParser.urlencoded({extended:true}))
+
+server.app.use('/', defaultRoutes);
+server.app.use('/videojuegos', gameRoutes);
+
+
+
+
+
+
+
 
 
 mongoose.connect('mongodb+srv://usr_videojuegos:videojuegos2022@cluster0.bseihec.mongodb.net/videojuegosDb',(error)=>{
